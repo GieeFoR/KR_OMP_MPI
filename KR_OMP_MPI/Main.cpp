d0b3f31611672945a22f98fa59fad33effed1f0f
GieeFoR <kamilooo1998@onet.pl>
2022-11-16 17:51:24 +0100

    Dodaj pliki projektów.

---
+++ KR_OMP_MPI/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <mpi.h>
+#include <stdio.h>
+#include <omp.h>
+
+
+int main() {
+
+	std::string chain = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+
+	std::cout << chain << std::endl;
+
+
+	return 0;
+}
